0f9d995ab74075c306b7c8b24ac930c4a93e76d0
regimantas <regtech0@gmail.com>
2017-12-19 22:30:11 +0200

    Fix for eps8266 and athers non avr mcu :)
    Fix for eps8266 and athers non avr mcu :)

--- Curve.h
+++ Curve.h
@@ -8,7 +8,11 @@
 #ifndef CURVE_H
 #define	CURVE_H
 
-#include <avr/pgmspace.h>
+#if (defined(__AVR__))
+#include <avr\pgmspace.h>
+#else
+#include <pgmspace.h>
+#endif
 
 class Curve {
  static const uint8_t etable[] PROGMEM;
